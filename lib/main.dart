193c248ebdadb1e685566fb5cc60a86787abe6de
AbhinabK <abhinabk123@gmail.com>
2021-08-14 15:25:37 +0530

    day3 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catelog/home_page.dart';
+import 'package:flutter_catelog/pages/home_page.dart';
+import 'package:flutter_catelog/pages/login_page.dart';
 
 void main(){
   runApp(MyApp());
@@ -8,9 +9,19 @@ void main(){
 
 class MyApp extends StatelessWidget {
   @override
+  // bulid return a Widget
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      
+      initialRoute: "/home",
+      routes:{
+        "/":(contex) => LoginPage(),
+        "/home":(contex) => HomePage(),
+        "/login":(contex) => LoginPage(),
+      } ,
     );
   }
 }
