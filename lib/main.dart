31b551d7f84e0f570909dd33f59b79db20fb41dc
AbhinabK <abhinabk123@gmail.com>
2021-08-14 15:58:21 +0530

    day4 stable

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.lightBlue),
       darkTheme: ThemeData(brightness: Brightness.dark),
       
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes:{
         "/":(contex) => LoginPage(),
         "/home":(contex) => HomePage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,7 +3,11 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-    
+      child: Column(
+        children: [
+        Image.asset("assets/images/login.png"),
+      ],
+      ),
     );
   }
 }
